845ee24834e8e2ad0dcbc6f21d00fc5570d706ee
robertclasing <robertclasing@gmail.com>
2022-02-13 14:41:41 -0300

    version2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+// Componentes
+import Navbar from'./Components/navbar/navbar'
+import ItemList from './Components/ItemListContainer/ItemList';
+// Estilos
+import './Components/styles/itemList.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <ItemList
+         dato1='Coders'
+      ></ItemList>
     </div>
   );
 }
---
+++ src/Components/ItemListContainer/ItemList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ItemList = (props) => {
+
+  return (
+    <div>
+        <h1>Hola{props.dato1}</h1>
+    </div>
+  )
+}
+
+export default ItemList
